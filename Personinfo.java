cd8d8e2a70aafc5ca3de26e5687dd66ea91d8cc9
Pawel <paw3lrod@gmail.com>
2023-01-16 23:53:49 +0100

    Lagde personinfo klassen og fullforte oppgaven

---
+++ Personinfo.java
@@ -0,0 +1,7 @@
+public class Personinfo {
+    public static void main(String[] args) {
+        System.out.println("Pawel");
+        System.out.println("12399199");
+        System.out.println("Holbergs gate");
+    }
+}
